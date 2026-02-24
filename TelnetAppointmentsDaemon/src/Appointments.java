2e797c1f73adf1a56425b71d671e0feb0f78fdbe
OtsileM <otsilemodiselle@outlook.com>
2026-02-24 22:47:06 +0200

    generate an appointments class

--- TelnetAppointmentsDaemon/src/Appointments.java
+++ TelnetAppointmentsDaemon/src/Appointments.java
@@ -1,2 +1,69 @@
+import java.time.LocalDateTime;
+
 public class Appointments {
+
+    private String id;
+    private LocalDateTime arrivalTime;
+    private String visitorName;
+    private String reason;
+    private boolean isConfirmed;
+
+    public Appointments(String id, LocalDateTime arrivalTime, String visitorName, String reason) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.visitorName = visitorName;
+        this.reason = reason;
+        this.isConfirmed = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public String getVisitorName() {
+        return visitorName;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public boolean isConfirmed() {
+        return isConfirmed;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setVisitorName(String visitorName) {
+        this.visitorName = visitorName;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        isConfirmed = confirmed;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointments{" +
+                "id='" + id + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", visitorName='" + visitorName + '\'' +
+                ", reason='" + reason + '\'' +
+                ", isConfirmed=" + isConfirmed +
+                '}';
+    }
 }
